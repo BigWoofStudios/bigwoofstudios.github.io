66ec291f6c79c8aafc13a25b71d62e0c7c2c782e
Jeffrey Rooks <onx2rj@gmail.com>
2024-01-15 23:31:17 -0500

    deploying

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git add .
 git commit -m "deploying"
 
 # force the push of the gh-pages branch to the remote gh-pages branch at origin
-git push -f origin gh-pages
+git push -f origin/gh-pages
 # delete the local temp because you will need it: ref
 git branch -D temp 
